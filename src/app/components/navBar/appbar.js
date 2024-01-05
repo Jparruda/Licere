d802ba71cd606ce4a22dc66329bdd23a193fd744
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2024-01-05 13:14:35 -0300

    appBar

---
+++ src/app/components/navBar/appbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navBar.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Minha Página Web</h1>
+      </header>
+      {/* Restante do conteúdo da sua página */}
+    </div>
+  );
+}
+
+export default App;
